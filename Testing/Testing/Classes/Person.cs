3963636c3a42915290449938154086f5c7e81104
Quinten <Quinten@QUINTEN-PC>
2017-04-11 10:45:03 +0200

    Testing interfaces, very simple example

---
+++ Testing/Testing/Classes/Person.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testing.Interfaces;
+
+namespace Testing.Classes
+{
+    public class Person : IMove
+    {
+        public Person()
+        {
+        }
+
+        public Person(string name) : this()
+        {
+            _name = name;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private string _name;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private bool _canMove;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool CanMove
+        {
+            get
+            {
+                return _canMove;
+            }
+
+            set
+            {
+                if (_canMove != value)
+                    _canMove = value;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public void Move()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+    }
+}
--- Testing/Testing/Program.cs
+++ Testing/Testing/Program.cs
@@ -3,23 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Testing.Classes;
+using Testing.Interfaces;
 
 namespace Testing
 {
-    class Program
+    public class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             try
             {
-                Console.WriteLine("Hello");
+                MoveObjects(new Car("Ferrari"));
+                MoveObjects(new Person("User"));
             }
             catch (Exception ex)
             {
-
                 Console.WriteLine("Error : " + ex.Message);
             }
 
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="movableObjetcs"></param>
+        private static void MoveObjects(IMove movableObjetcs)
+        {
+            movableObjetcs.Move();
         }
     }
 }
