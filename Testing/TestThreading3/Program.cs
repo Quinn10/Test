d165b684004d057938e0e55de2454053dd3a3acf
quintenbeukes <quintenbeukes@outlook.com>
2017-11-10 13:56:40 +0200

    fwefwe

--- Testing/TestThreading3/Program.cs
+++ Testing/TestThreading3/Program.cs
@@ -11,12 +11,30 @@ namespace TestThreading3
         static void Main(string[] args)
         {
             InitTask.QueueAction(Test);
+            InitTask.QueueAction(Test2);
+
+            Console.ReadLine();
+        }
+
+        private static void Test2()
+        {
+            Console.WriteLine("THis is a second test");
+
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine("Console : " + i);
+            }            
         }
 
         public static void Test()
         {
-            Console.WriteLine("THis is a test");
-        }
 
+            Console.WriteLine("THis is a test");
+
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine("Console : " + i);
+            }            
+        }
     }
 }
